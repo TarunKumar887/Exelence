f092e1734dc5b5091512b75b6a6e4a27e075aa06
TarunKumar887 <tarunk6952@gmail.com>
2025-09-06 21:46:22 +0530

    Fix CORS: allow deployed frontend domain

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ connectDB();
 
 // Enhanced CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://exelencefrontend.onrender.com', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'https://exelencefrontend.onrender.com','https://exelence-zoao.onrender.com', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
